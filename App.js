55478c81e0f4218ba29c05559a4ff2701009025b
Myles <mazehko@alphaworks.tech>
2021-10-07 17:47:20 -0700

    React demo from 10/7/2021

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Button
+        title="Open Dialog"/>
+        <View style={styles.dialogContainer}>
+          <Text>Choose Okay or Cancel</Text>
+          <View>
+            <Button title="Okay" />
+            <Button title="Cancel" />
+          </View>
+        </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +27,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  dialogContainer: {
+    border: 'solid 1px #FF3399',
+    padding: 30,
+  },
+  buttonActions: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+  }
+
 });
